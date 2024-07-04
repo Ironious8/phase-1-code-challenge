b6d09b1fbc8adf13860d28244b50cc82fdadfec0
Ironious8 <tyronekariuki@gmail.com>
2024-07-04 14:39:13 +0300

    Modified salary

--- salary.js
+++ salary.js
@@ -12,70 +12,71 @@ let grossPay=basicSalary+benefits;
 // PAYE calculation
 let paye=10000;
 if (taxableIncome<=24000) {
-    return 'paye=taxableIncome*0.1';
+    console.log ('paye=taxableIncome*0.1');
 }
 else if (taxableIncome<=32333) {
-    return 'paye=24000*0.1+(taxableIncome-24000)*0.25';
+    console.log('paye=24000*0.1+(taxableIncome-24000)*0.25');
 }
 else if (taxableIncome<=500000) {
-    return 'paye=24000*0.1+8333*0.25(taxableIncome-32333)*0.3';
+    console.log('paye=24000*0.1+8333*0.25(taxableIncome-32333)*0.3');
 }
 else if (taxableIncome<=800000) {
-    return 'paye=24000*0.1+8333*0.25+41667*0.03(taxableIncome-500000)*0.325';
+    console.log('paye=24000*0.1+8333*0.25+41667*0.03(taxableIncome-500000)*0.325');
 }
 else {
     console.log('paye=24000*0.1+8333*0.25+41667*0.03+32333*0.325(taxableIncome-800000)*0.35');
 }
-  console.log(taxableIncome);
+  
 
 //NHIF calculation
+let nhif=1500;
 if(grossPay<=5999) {
-    return 'nhif=150';
+    console.log('nhif=150');
 }
 else if(grossPay<=7999) {
-    return 'nhif=300';
+    console.log('nhif=300');
 }    
 else if(grossPay<=11999) {
-    return 'nhif=400';
+    console.log('nhif=400');
 }    
 else if (grossPay<=14999) {
-    return 'nhif=500';
+    console.log('nhif=500');
 }
 else if(grossPay<=19999) {
-    return 'nhif=600';
+    console.log('nhif=600');
 }
 else if(grossPay<=24999) {
-    return 'nhif=750';
+    console.log('nhif=750');
 }
 else if(grossPay<=29999) {
-    return 'nhif=850';
+    console.log('nhif=850');
 }
 else if(grossPay<=34999) {
-    return 'nhif=900';
+    console.log('nhif=900');
 }
 else if(grossPay<=39999) {
-    return 'nhif=950';
+    console.log('nhif=950');
 }
 else if(grossPay<=44999) {
-    return 'nhif=1000';
+    console.log('nhif=1000');
 }
 else if(grossPay<=49999) {
-    return 'nhif=1100';
+    console.log('nhif=1100');
 }
 else if(grossPay<=59999) {
-    return 'nhif=1200';
+    console.log('nhif=1200');
 }
 else if(grossPay<=69999) {
-    return 'nhif=1300';
+    console.log('nhif=1300');
 }
 else if(grossPay<=79999) {
-    return 'nhif=1400';
+    console.log('nhif=1400');
 }
 else if(grossPay<=89999) {
-    return 'nhif=1500';
+    console.log('nhif=1500');
 }
 else if(grossPay<=99999) {
-    return 'nhif=1600';
+    console.log('nhif=1600');
 }
 else {
     console.log ('nhif=1700');
@@ -84,7 +85,7 @@ else {
 //NSSF calculation
 let tier1=Math.min(grossPay, 7000)*0.06;
  let tier2=Math.min(grossPay-7000, 29000)*0.06;
- let nssf=tier1+tier2;
+ let nssf=`${tier1}+${tier2}`;
     
 
  // Net salary calculation
